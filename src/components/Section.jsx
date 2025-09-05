99ea030cb53082b155c778b105f8c01c192eefc8
Malik Vance <malikovance@gmail.com>
2025-09-05 12:39:39 -0500

    added section.jsx so i can pass a title prop and any children in the component

---
+++ src/components/Section.jsx
@@ -0,0 +1,13 @@
+//Section.jsx
+import React from "react";
+
+const Section = ({ title, children }) => {
+  return (
+    <div className="section">
+      <h1>{title}</h1>
+      {children}
+    </div>
+  );
+};
+
+export default Section;
