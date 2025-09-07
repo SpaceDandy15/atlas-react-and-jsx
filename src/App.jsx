399dd168f3f67d71b59b49179d7e8b6e8d568c5f
Malik Vance <malikovance@gmail.com>
2025-09-07 10:57:13 -0500

    updated app.jsx with what is react and benefits of react.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 // src/App.jsx
 import React from "react";
 import Header from "./components/Header";
+import Section from "./components/Section";
 
 const App = () => {
   return (
     <div>
       <Header />
+
+      {/* What is React Section */}
+      <Section title="What is react?">
+        <p>
+          Morbi sed semper arcu, sed ullamcorper mauris. Integer nec turpis ipsum. Proin scelerisque sagittis rutrum. Nam ut tellus sagittis, scelerisque tortor, vitae, suscipit ex. Ut ut lacus sit amet eros consequat pulvinar ornare in odio. Nulla eleifend laoreet lacus, ut fringilla purus dapibus nec.
+        </p>
+      </Section>
+
+      {/* Benefits of React Section */}
+      <Section title="Benefits of react">
+        <ul>
+          <li>Maecenas a tortor laoreet</li>
+          <li>Orci varius natoque</li>
+          <li>Donec vitae magna</li>
+          <li>Vivamus sit amet</li>
+          <li>Proin lulla lectus</li>
+        </ul>
+      </Section>
     </div>
   );
 };
